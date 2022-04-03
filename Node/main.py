b5aeb71a48d551da8d15b7125182f4cade5d4244
Priya Sonawane <priyasanjay0416@gmail.com>
2022-04-03 18:26:09 -0400

    added feilds for persistent data

--- Node/main.py
+++ Node/main.py
@@ -4,11 +4,21 @@ import os
 from Node.heartbeat import AppendEntryMessage
 import heartbeat
 import nodes
+#persistent data
+voted_for = None
+which_term = 0
+Log =[]
 
+
+
+
+# references: https://docs.python.org/3/howto/sockets.html
 def send_heartbeat(name):
     msg = AppendEntryMessage({ "leaderId": name,"Entries":[],"prevLogIndex":-1,"prevLogTerm":-1})
     # need to figure out how to send info to other threads 
     # for x in nodes.nodes:
     #     if x.name != name:
     #         print("sedning to follower node")
-        
+
+if __name__ =='__main__':
+
